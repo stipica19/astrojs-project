152b53d269f88149a06df50318178f4313d07994
stipica19 <pipiklepic1@gmail.com>
2026-02-18 19:59:28 +0100

    ikonice nove

---
+++ src/i18n/featureIcons.ts
@@ -0,0 +1,6 @@
+import { Accessibility, Globe, Code, ShieldCheck } from "@lucide/astro";
+
+export const featureIcons = {
+    mission: [ShieldCheck, Code, Accessibility],
+    products: [Globe],
+};
