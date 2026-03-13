ca228e6b13f3b427a407f110e9d1fe6e0fa0792b
stipica19 <pipiklepic1@gmail.com>
2026-03-13 20:30:09 +0000

    fix dizajn v1

--- src/i18n/featureIcons.ts
+++ src/i18n/featureIcons.ts
@@ -14,6 +14,7 @@ import {
 } from "@lucide/astro";
 
 export const featureIcons = {
+    services: [Monitor, Wrench, Network],
     mission: [ShieldCheck, Code, Accessibility],
     products: [Globe],
     webLabWhatWeDo: [Monitor, Network, Gauge, Sliders],
